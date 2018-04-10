153d7e1edc6a5c7929741ee41980c83c63940ed1
WanchaloemMacBookPro <wanchaloemmacbookpro@WanchaloemMacBookPros-MacBook-Pro.local>
2018-04-10 22:25:03 +0700

    update title webpage

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>CodeIgniter Tranning</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<style type="text/css">
 		body {
