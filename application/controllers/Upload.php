311d86922b437c52266c6c9d002c17e69de4b912
WanchaloemMacBookPro <wanchaloemmacbookpro@WanchaloemMacBookPros-MacBook-Pro.local>
2018-04-21 22:27:41 +0700

    upload multiple file

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -26,4 +26,48 @@ class Upload extends CI_Controller {
             'error' => $this->upload->display_errors()
         ]);
     }
+
+    public function upload_multiple_file()
+    {
+        $upload_path = APPPATH . 'uploads';
+        if(!file_exists($upload_path)) mkdir($upload_path);
+        if(!$_FILES) redirect(base_url('upload'));
+        $this->load->library('upload', [
+            'upload_path' => $upload_path,
+            'allowed_types' => 'jpg'
+        ]);
+        $uploads = [];
+        $files = $_FILES;
+        $count = count($_FILES['file']['name']);
+        // วนลูปหาแต่ล่ะไฟล์ อัพโหลด
+        for($i = 0; $i < $count; $i++)
+        {
+            $_FILES['file']['name'] = $files['file']['name'][$i];
+            $_FILES['file']['type'] = $files['file']['type'][$i];
+            $_FILES['file']['tmp_name'] = $files['file']['tmp_name'][$i];
+            $_FILES['file']['size'] = $files['file']['size'][$i];
+            $_FILES['file']['error'] = $files['file']['error'][$i];
+            if($this->upload->do_upload('file')) 
+            {
+                // ถ้าไม่มีข้อผิดพลาด
+                $uploads[] = [
+                    'error' => false,
+                    'file_name' => $this->upload->data()['file_name'],
+                    'message' => null
+                ];
+            }
+            else 
+            {
+                // ถ้าเกิดข้อผิดพลาด
+                $uploads[] = [
+                    'error' => true,
+                    'file_name' => $_FILES['file']['name'],
+                    'message' => $this->upload->display_errors('', '')
+                ];
+            }
+        }
+        $this->load->view('upload', [
+            'uploads' => $uploads
+        ]);
+    }
 }
--- application/views/upload.php
+++ application/views/upload.php
@@ -13,12 +13,14 @@
         <div class="card">
             <h5 class="card-header">Codeigniter การอัพโหลดไฟล์แบบไฟล์เดียว</h5>
             <div class="card-body">
-                <form action="<?= base_url('upload/upload_file') ?>" method="post" enctype="multipart/form-data">
+                <form action="<?= base_url('upload/upload_multiple_file') ?>" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <div id="file_text" class="alert alert-warning">กรุณาอัพโหลดไฟล์</div>
+                    </div>
                     <div class="form-group">
                         <label class="btn btn-warning" style="margin: 0">
                             เลือกรูปภาพอัพโหลด
-                            <span id="file_text"></span>
-                            <input type="file" onchange="file_text.innerText = this.value" hidden name="file">
+                            <input multiple type="file" onchange="show_upload_filename(this)" hidden name="file[]">
                         </label>
                         <input type="submit" value="อัพโหลดภาพ" class="btn btn-primary">
                     </div>
@@ -30,9 +32,59 @@
                     </a>
                 <?php elseif(isset($error)) : ?>
                     <div style="color: #aa0000"><?= $error ?></div>
+                <?php elseif(isset($uploads)) : ?>
+                    <!-- Upload แบบหลายไฟล์ -->
+                    <div class="row">
+                        <?php foreach($uploads as $data) : ?>
+                            <div class="col-sm-3">
+                                <div class="form-group">
+                                    <div class="card">
+                                        <!-- ไม่มีข้อผิดพลาด -->
+                                        <?php if($data['error'] == false) : ?>
+                                            <img src="<?= base_url("application/uploads/{$data['file_name']}") ?>" class="card-img-top">
+                                            <div class="card-body">
+                                                <div class="card-text">
+                                                    <div><?= $data['file_name'] ?></div>
+                                                    <a target="_blank" href="<?= base_url("application/uploads/{$data['file_name']}") ?>">
+                                                        ดูภาพขนาดเต็ม
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        <!-- เกิดข้อผิดพลาด -->
+                                        <?php else : ?>
+                                            <img src="http://www.pbs.org/black-culture/lunchbox_plugins/s/photogallery/img/no-image-available.jpg" class="card-img-top">
+                                            <div class="alert alert-danger" style="border-radius: 0; border: none">
+                                                <p><?= $data['file_name'] ?></p>
+                                                <strong>Error : </strong>
+                                                <span><?= $data['message'] ?></span>
+                                            </div>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
                 <?php endif; ?>
             </div>
         </div>
     </div>
+
+    <!-- javascript -->
+    <script>
+        function show_upload_filename(input)
+        {
+            var files = input.files;
+            var filename = [];
+            // ตรวจสอบว่ามีการอัพโหลดหรือไม่
+            if(files.length > 0)
+            {
+                // วนลูปเพื่อเก็บชื่อไฟล์
+                for(var i = 0; i < files.length; i++)
+                    filename.push(files[i].name); 
+            }
+            // นำชื่อไฟล์ที่เป็น Array มาต่อสตริงคั่นด้วย <br />
+            file_text.innerHTML = filename.join('<br />');
+        }
+    </script>
 </body>
 </html>
